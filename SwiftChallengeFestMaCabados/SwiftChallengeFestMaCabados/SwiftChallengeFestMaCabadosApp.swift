30c52a39c4bada6a046edcaba9b53c5d520309b5
Rodrigo López Murguía <A01742780@tec.mx>
2025-06-07 12:29:20 -0600

    app create

---
+++ SwiftChallengeFestMaCabados/SwiftChallengeFestMaCabados/SwiftChallengeFestMaCabadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallengeFestMaCabadosApp.swift
+//  SwiftChallengeFestMaCabados
+//
+//  Created by Rocco López on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallengeFestMaCabadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
